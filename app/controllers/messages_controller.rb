b0a24688ccde929b336dd364f7364c024661f7d7
unaibot <unaibot@gmail.com>
2017-05-24 03:55:17 +0200

    Delete/Edit message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,6 @@
 class MessagesController < ApplicationController
 
-before_action :find_message, only: [:show, :edit]
+before_action :find_message, only: [:show, :edit, :update, :destroy]
 
 	def index		
 		@messages = Message.all.order("created_at DESC")
@@ -21,6 +21,22 @@ before_action :find_message, only: [:show, :edit]
 		else
 			render 'new'
 		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @message.update(message_params)
+			redirect_to message_path
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@message.destroy
+		redirect_to root_path
 	end
 
 	private
